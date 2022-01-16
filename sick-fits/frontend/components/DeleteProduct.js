77f5193bd041bf135217b950ac2b14dc99fe4307
Shaun Hartman <shaunh@teamsi.com>
2022-01-16 10:43:00 -0600

    Delete Product

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm('Are you sure you want to delete this item?')) {
+          // go ahead & delete it
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+DeleteProduct.propTypes = {
+  children: PropTypes.any,
+  id: PropTypes.string,
+};
