d174e6dff924b2215f58b5c6e2bcb493b7b3d3b9
Shaun Hartman <shaunh@teamsi.com>
2022-01-19 11:02:23 -0600

    finishing pagination

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/client';
 import styled from 'styled-components';
 import Product from './Product';
+import { perPage } from '../config';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -26,8 +28,14 @@ const ProductsListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products({ page }) {
+  console.log('type of page', typeof page);
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -44,3 +52,7 @@ export default function Products() {
     </div>
   );
 }
+
+Products.propTypes = {
+  page: PropTypes.any,
+};
